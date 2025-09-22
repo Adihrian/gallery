4a0c1ce5e83ad505c9dda8bfee2aeb6583c5570c
Adihrian <aapindi03@gmail.com>
2025-09-22 13:07:30 +0300

    Testing the connection of the DB

--- testMongo.js
+++ testMongo.js
@@ -10,11 +10,11 @@ mongoose.connect(config.mongoURI[env], {
     useUnifiedTopology: true,
 })
 .then(() => {
-    console.log(`✅ Successfully connected to MongoDB (${env})`);
+    console.log(`Successfully connected to MongoDB (${env})`);
     return mongoose.connection.close();
 })
 .catch((err) => {
-    console.error('❌ Error connecting to MongoDB:', err.message);
+    console.error('Error connecting to MongoDB:', err.message);
 });
 
 
